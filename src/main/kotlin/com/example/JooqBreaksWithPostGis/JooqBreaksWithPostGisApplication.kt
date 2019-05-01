a1e57c54d2c271c434ef6a7cd9b9864cc68b182b
Raj <the.raj.saxena@gmail.com>
2019-05-01 17:25:00 +0200

    Init commit

---
+++ src/main/kotlin/com/example/JooqBreaksWithPostGis/JooqBreaksWithPostGisApplication.kt
@@ -0,0 +1,11 @@
+package com.example.JooqBreaksWithPostGis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JooqBreaksWithPostGisApplication
+
+fun main(args: Array<String>) {
+	runApplication<JooqBreaksWithPostGisApplication>(*args)
+}
